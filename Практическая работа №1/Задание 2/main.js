535877db506a1d57d6736d6b6ff4cd767b95989d
TheMusZero <Fedorovvladislav030@gmail.com>
2024-06-22 17:45:57 +0300

    Задание 2

--- Практическая работа №1/Задание 2/main.js
+++ Практическая работа №1/Задание 2/main.js
@@ -1,10 +1,5 @@
 function greet() {
-    // Вывод приветственного сообщения в диалоговом окне
     alert("Добро пожаловать!");
-
-    // Вывод приветственного сообщения в окно браузера
     document.getElementById("welcomeMessage").innerText = "Добро пожаловать!";
 }
-
-// Вызов функции greet() при загрузке страницы
 window.onload = greet;
